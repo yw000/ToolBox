8133f4f029e474237ee499c82845cf34a128cc4d
yw000 <yw_0217@163.com>
2016-03-18 18:03:22 +0800

    先做目录

--- app/src/main/java/com/yang/toolbox/MainActivity.java
+++ app/src/main/java/com/yang/toolbox/MainActivity.java
@@ -1,13 +1,27 @@
 package com.yang.toolbox;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        initView();
+    }
+
+    private void initView() {
+        toolbar.setTitle(getResources().getString(R.string.app_name));
+        setSupportActionBar(toolbar);
     }
 }
